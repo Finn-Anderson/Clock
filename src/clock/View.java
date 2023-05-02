71156fd4aea724e840b783b95ba1f086376a3a42
FereWulf <finn@toxicflames.co.uk>
2023-05-02 12:01:40 +0100

    Added jdk

--- src/clock/View.java
+++ src/clock/View.java
@@ -1,9 +1,9 @@
 package clock;
 
-import java.awt.*;
 import javax.swing.*;
-import java.util.Observer;
+import java.awt.*;
 import java.util.Observable;
+import java.util.Observer;
 
 public class View implements Observer {
     
