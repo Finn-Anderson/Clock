0c2239e873941a9c472f809476bf924b9b12ed66
FereWulf <finn@toxicflames.co.uk>
2023-05-08 01:27:35 +0100

    JSpinner UI for editting/deleting/adding alarms

--- src/clock/View.java
+++ src/clock/View.java
@@ -5,11 +5,13 @@ import priorityqueues.QueueUnderflowException;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
+import javax.swing.text.DateFormatter;
 import java.awt.*;
 import java.io.File;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.*;
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
 public class View implements Observer {
     
@@ -65,7 +67,7 @@ public class View implements Observer {
                     return true;
                 } else {
                     String filename = f.getName().toLowerCase();
-                    return filename.endsWith(".ics") || filename.endsWith(".ics") ;
+                    return filename.endsWith(".ics") ;
                 }
             }
         });
@@ -102,16 +104,20 @@ public class View implements Observer {
                 btn.setAlignmentX(Component.CENTER_ALIGNMENT);
                 box.add(btn);
                 box.add(Box.createVerticalStrut(10));
+
+                btn.addActionListener(ex -> selectAlarm(alarmPriorityItem.getItem().getDate(), alarmPriorityItem.getItem().getSummary()));
             }
 
-            JButton newAlarm = new JButton("New");
-            newAlarm.setAlignmentX(Component.CENTER_ALIGNMENT);
-            box.add(newAlarm, BorderLayout.PAGE_END);
+            JButton btn = new JButton("New");
+            btn.setAlignmentX(Component.CENTER_ALIGNMENT);
+            box.add(btn, BorderLayout.PAGE_END);
+
+            btn.addActionListener(ex -> selectAlarm(new Date(), ""));
 
             alarmListWindow.setVisible(true);
         });
 
-        // Menu bar w/ alarm button and countdown timer
+        // Menu bar w/ alarm button and countdown timer.
         JPanel menu = new JPanel();
         pane.add(menu, BorderLayout.PAGE_END);
         menu.add(alarms);
@@ -143,11 +149,9 @@ public class View implements Observer {
         closeBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
         popupBox.add(closeBtn);
 
-        closeBtn.addActionListener(e -> {
-            alarmPopup.dispose();
-        });
+        closeBtn.addActionListener(e -> alarmPopup.dispose());
 
-        // Save to folder/file
+        // Save to folder/file.
         frame.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
@@ -171,7 +175,7 @@ public class View implements Observer {
                             return true;
                         } else {
                             String filename = f.getName().toLowerCase();
-                            return filename.endsWith(".ics") || filename.endsWith(".ics") ;
+                            return filename.endsWith(".ics") ;
                         }
                     }
                 });
@@ -196,6 +200,142 @@ public class View implements Observer {
         frame.pack();
         frame.setVisible(true);
     }
+
+    /** Displays the ui to add/edit/remove an alarm.
+     *
+     * @param date takes date of selected alarm.
+     * @param summary takes summary of selected alarm.
+     */
+    public void selectAlarm(Date date, String summary) {
+        JDialog alarmWindow = new JDialog();
+        alarmWindow.setSize(new Dimension(300, 200));
+        alarmWindow.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+        Box box = Box.createVerticalBox();
+        alarmWindow.add(box);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        Calendar currentDate = Calendar.getInstance();
+        currentDate.setTime(new Date());
+
+        // Year spinner.
+        JPanel yearContainer = new JPanel();
+        box.add(yearContainer);
+
+        JLabel yearLabel = new JLabel("Year:");
+
+        JSpinner yearSpinner = new JSpinner();
+        yearSpinner.setAlignmentX(Component.CENTER_ALIGNMENT);
+        yearSpinner.setValue(calendar.get(Calendar.YEAR));
+
+        JSpinner.NumberEditor editor = new JSpinner.NumberEditor(yearSpinner, "#");
+        yearSpinner.setEditor(editor);
+
+        yearSpinner.addChangeListener(e -> {
+            int value = (int) yearSpinner.getValue();
+            int minYear = currentDate.get(Calendar.YEAR);
+
+            if (value < minYear) {
+                yearSpinner.setValue(minYear);
+            }
+        });
+
+        yearContainer.add(yearLabel);
+        yearLabel.setLabelFor(yearSpinner);
+        yearContainer.add(yearSpinner);
+
+        // Month spinner.
+        JPanel monthContainer = new JPanel();
+        box.add(monthContainer);
+
+        JLabel monthLabel = new JLabel("Month:");
+
+        int month = (calendar.get(Calendar.MONTH) + 1);
+        SpinnerNumberModel monthModel = new SpinnerNumberModel(month, 1, 12, 1);
+        JSpinner monthSpinner = new JSpinner(monthModel);
+        monthSpinner.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        monthSpinner.addChangeListener(e -> {
+            int value = (int) monthSpinner.getValue();
+            int yearValue = (int) yearSpinner.getValue();
+            int minMonth = (currentDate.get(Calendar.MONTH) + 1);
+            int minYear = currentDate.get(Calendar.YEAR);
+
+            if (value < minMonth && yearValue == minYear) {
+                monthSpinner.setValue(minMonth);
+            }
+        });
+
+        monthContainer.add(monthLabel);
+        monthLabel.setLabelFor(monthSpinner);
+        monthContainer.add(monthSpinner);
+
+        // Day spinner.
+        JPanel dayContainer = new JPanel();
+        box.add(dayContainer);
+
+        JLabel dayLabel = new JLabel("Day:");
+
+        YearMonth yearMonth = YearMonth.of((int) yearSpinner.getValue(), (int) monthSpinner.getValue());
+        int daysInMonth = yearMonth.lengthOfMonth();
+        SpinnerNumberModel dayModel = new SpinnerNumberModel(calendar.get(Calendar.DAY_OF_MONTH), 1, daysInMonth, 1);
+        JSpinner daySpinner = new JSpinner(dayModel);
+        daySpinner.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        daySpinner.addChangeListener(e -> {
+            int value = (int) daySpinner.getValue();
+            int monthValue = (int) monthSpinner.getValue();
+            int yearValue = (int) yearSpinner.getValue();
+            int minDay = currentDate.get(Calendar.DAY_OF_MONTH);
+            int minMonth = (currentDate.get(Calendar.MONTH) + 1);
+            int minYear = currentDate.get(Calendar.YEAR);
+
+            if (value < minDay && monthValue == minMonth && yearValue == minYear) {
+                daySpinner.setValue(minDay);
+            }
+        });
+
+        dayContainer.add(dayLabel);
+        dayLabel.setLabelFor(daySpinner);
+        dayContainer.add(daySpinner);
+
+        // Time spinner.
+        JPanel timeContainer = new JPanel();
+        box.add(timeContainer);
+
+        JLabel timeLabel = new JLabel("Time:");
+
+        SpinnerDateModel timeModel = new SpinnerDateModel(date, null, null, Calendar.MINUTE);
+        JSpinner timeSpinner = new JSpinner(timeModel);
+        timeSpinner.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JSpinner.DateEditor timeEditor = new JSpinner.DateEditor(timeSpinner, "HH:mm");
+        timeEditor.getTextField().setEditable( false );
+        timeSpinner.setEditor(timeEditor);
+
+        timeSpinner.addChangeListener(e -> {
+            Calendar checkTime = Calendar.getInstance();
+            checkTime.setTime((Date) timeSpinner.getValue());
+            checkTime.set(Calendar.DAY_OF_MONTH, (int) daySpinner.getValue());
+            checkTime.set(Calendar.MONTH, ((int) monthSpinner.getValue()) - 1);
+            checkTime.set(Calendar.YEAR, (int) yearSpinner.getValue());
+
+            System.out.print(checkTime.compareTo(currentDate) + "\r\n");
+
+            if (checkTime.compareTo(currentDate) < 0) {
+                Date current = new Date();
+                current.setMinutes(current.getMinutes() + 1);
+                timeSpinner.setValue(current);
+            }
+        });
+
+        timeContainer.add(timeLabel);
+        timeLabel.setLabelFor(timeSpinner);
+        timeContainer.add(timeSpinner);
+
+        alarmWindow.setVisible(true);
+    }
     
     public void update(Observable o, Object arg) {
         panel.repaint();
@@ -209,3 +349,4 @@ public class View implements Observer {
         }
     }
 }
+
